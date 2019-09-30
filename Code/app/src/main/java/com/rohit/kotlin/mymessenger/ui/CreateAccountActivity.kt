0984f8ad546bdd8a3c3548730929d87c83524d90
Rohit Joshi <rohit.joshi@tieto.com>
2019-09-30 14:49:22 +0530

    Added placeholders for login & create account activity.

---
+++ Code/app/src/main/java/com/rohit/kotlin/mymessenger/ui/CreateAccountActivity.kt
@@ -0,0 +1,12 @@
+package com.rohit.kotlin.mymessenger.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CreateAccountActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_account)
+    }
+}
--- Code/app/src/main/java/com/rohit/kotlin/mymessenger/ui/MainActivity.kt
+++ Code/app/src/main/java/com/rohit/kotlin/mymessenger/ui/MainActivity.kt
@@ -1,13 +1,20 @@
 package com.rohit.kotlin.mymessenger.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rohit.kotlin.mymessenger.R
+import kotlinx.android.synthetic.main.activity_login.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnAlreadyHaveAccount.setOnClickListener {
+            startActivity(Intent(this, LoginActivity::class.java))
+        }
     }
 }
