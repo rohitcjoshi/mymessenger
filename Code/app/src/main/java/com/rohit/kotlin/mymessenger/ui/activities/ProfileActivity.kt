a45a142d5772b5d8717f06ccda831d44a99c2eb6
Rohit Joshi <rohitcjoshi.4@gmail.com>
2019-11-04 13:48:51 +0530

    added profile activity.

--- Code/app/src/main/java/com/rohit/kotlin/mymessenger/ui/activities/ProfileActivity.kt
+++ Code/app/src/main/java/com/rohit/kotlin/mymessenger/ui/activities/ProfileActivity.kt
@@ -6,19 +6,27 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.*
 import com.rohit.kotlin.mymessenger.R
-import com.rohit.kotlin.mymessenger.utils.KEY_DB_USERS
-import com.rohit.kotlin.mymessenger.utils.KEY_INTENT_USER_ID
+import com.rohit.kotlin.mymessenger.ui.fragments.LoadingDialog
+import com.rohit.kotlin.mymessenger.utils.*
+import com.squareup.picasso.Callback
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_profile.*
+import kotlinx.android.synthetic.main.activity_settings.*
+import java.lang.Exception
 
 class ProfileActivity : AppCompatActivity() {
     var currentUser: FirebaseUser? = null
     var userId: String? = null
     var userDatabaseRef: DatabaseReference? = null
+    var progressBar: LoadingDialog? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
         supportActionBar!!.title = "Profile"
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        progressBar = LoadingDialog(this)
 
         if(intent.extras != null) {
             userId = intent.extras?.get(KEY_INTENT_USER_ID).toString()
@@ -32,10 +40,29 @@ class ProfileActivity : AppCompatActivity() {
     }
 
     private fun setupUserProfile() {
+        progressBar?.showProgressDialog()
         userDatabaseRef?.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapShot: DataSnapshot) {
+                val displayName = snapShot.child(KEY_INTENT_DISPLAY_NAME).value.toString()
+                val status = snapShot.child(KEY_DB_CHILD_STATUS).value.toString()
+                val image = snapShot.child(KEY_INTENT_IMAGE).value.toString()
+
+                profileDisplayNameTxt.text = displayName
+                profileStatusTxt.text = status
+                Picasso.get()
+                    .load(image)
+                    .placeholder(R.drawable.happy_woman)
+                    .into(profileImageView, object: Callback {
+                        override fun onSuccess() {
+                            progressBar?.dismissProgressDialog()
+                        }
+                        override fun onError(e: Exception?) {
+                            progressBar?.dismissProgressDialog()
+                        }
+                    })
             }
             override fun onCancelled(error: DatabaseError) {
+                progressBar?.dismissProgressDialog()
             }
         })
     }
