58f823662d19b3b3c0e62d2d5aec618a85186004
Rohit Joshi <rohit.joshi@tieto.com>
2019-11-07 08:27:20 +0530

    cleanup

--- Code/app/src/main/java/com/rohit/kotlin/mymessenger/ui/adapters/SectionPageAdapter.kt
+++ Code/app/src/main/java/com/rohit/kotlin/mymessenger/ui/adapters/SectionPageAdapter.kt
@@ -3,10 +3,11 @@ package com.rohit.kotlin.mymessenger.ui.adapters
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
+import androidx.fragment.app.FragmentStatePagerAdapter
 import com.rohit.kotlin.mymessenger.ui.fragments.ChatsFragment
 import com.rohit.kotlin.mymessenger.ui.fragments.UsersFragment
 
-class SectionPageAdapter(fm: FragmentManager): FragmentPagerAdapter(fm) {
+class SectionPageAdapter(fm: FragmentManager): FragmentStatePagerAdapter(fm, BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT) {
     override fun getItem(position: Int): Fragment {
         when(position) {
             0 -> {
